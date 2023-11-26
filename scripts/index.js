f1fb1f50c516e543e6e99b111742cabb5ee2763f
Roger Rajaratnam <roger@sourcier.uk>
2023-11-26 13:16:41 -0800

    add hello world function and call it from the frontend

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
-const messageNode = document.elementById("message")
+const messageNode = document.getElementById("message")
+
+const render = (message = "", DOMNode = messageNode) => {
+    DOMNode.textContent = message
+}
+
+const getData = async () => {
+    try {
+        const response = await fetch('/.netlify/functions/hello-world')
+        if(!response.ok) throw response
+        const data = await response.json()
+        render(data.message)
+    } catch (err) {
+        alert("Error fetching. Check console")
+        console.error(err)
+    }
+}
+
+getData()
